b3a025bc9c6c3ee14581b7420b9a30b3ef4c9005
Talha2195 <143935671+Talha2195@users.noreply.github.com>
2024-08-14 18:34:34 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+      filename: "index.html",
+      template: "src/temp.html",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+}
